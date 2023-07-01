9f36d36bf2a8921cd7034e87ec1cf7acb8565aa0
Kaiki-kk <kitami@purpom-media-lab.com>
2023-07-01 18:40:46 +0900

    Initial commit

---
+++ test/daily-report-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DailyReportApp from '../lib/daily-report-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/daily-report-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DailyReportApp.DailyReportAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
